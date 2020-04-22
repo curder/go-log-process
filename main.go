a091b01313e082cbc657e2d89cc3be8319156f45
curder <curder@foxmail.com>
2020-04-23 01:46:29 +0800

    main

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
     "bufio"
     "context"
+    "encoding/json"
     "flag"
     "fmt"
     "github.com/influxdata/influxdb-client-go"
     "io"
     "log"
+    "net/http"
     "net/url"
     "os"
+    "os/signal"
     "regexp"
     "strconv"
     "strings"
+    "syscall"
     "time"
 )
 
@@ -23,8 +27,10 @@ func main() {
         lp        *LogProcess
         r         Reader
         w         Writer
-        rc        = make(chan []byte)
-        wc        = make(chan Message)
+        rc        = make(chan []byte, 200)
+        wc        = make(chan *Message, 200)
+        monitor   *Monitor
+        i         int
     )
 
     flag.StringVar(&path, "path", "./access.log", "read file path")
@@ -36,10 +42,30 @@ func main() {
     lp = NewLogProcess(rc, wc, r, w)
 
     go lp.read.Read(lp.rc)
-    go lp.Process()
-    go lp.write.Write(lp.wc)
+    for i = 0; i < 2; i++ {
+        go lp.Process()
+    }
+    for i = 0; i < 4; i++ {
+        go lp.write.Write(lp.wc)
+    }
 
-    time.Sleep(360000 * time.Second)
+    // 监控模块
+    monitor = NewMonitor(time.Now(), SystemInfo{})
+    go monitor.start(lp)
+
+    c := make(chan os.Signal)
+    signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGUSR1)
+    for s := range c {
+        switch s {
+        case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+            log.Println("capture exit signal:", s)
+            os.Exit(1)
+        case syscall.SIGUSR1: // 用户自定义信号
+            log.Println(lp)
+        default:
+            log.Println("capture other signal:", s)
+        }
+    }
 }
 
 // 读取接口
@@ -75,6 +101,7 @@ func (r *ReadFromFile) Read(rc chan []byte) {
     if _, err = file.Seek(0, 2); err != nil {
         panic(fmt.Sprintf("read file err: %s", err.Error()))
     }
+    // TODO 处理日志切割的问题
     rd = bufio.NewReader(file)
     for {
         line, err = rd.ReadBytes('\n')
@@ -84,13 +111,16 @@ func (r *ReadFromFile) Read(rc chan []byte) {
         } else if err != nil {
             panic(fmt.Sprintf("Read bytes err: %s", err.Error()))
         }
+
+        TypeMonitorChan <- TypeHandleLine
+
         rc <- line[:len(line)-1] // 模拟往 ReadChan中写入一行数据，删除末尾的换行符
     }
 }
 
 // 写入接口
 type Writer interface {
-    Write(wc chan Message)
+    Write(wc chan *Message)
 }
 
 // 写入到fluxDB
@@ -106,12 +136,12 @@ func NewWriteToFluxDB(influxDBDNS string) *WriteToFluxDB {
 }
 
 // 写入模块
-func (w *WriteToFluxDB) Write(wc chan Message) {
+func (w *WriteToFluxDB) Write(wc chan *Message) {
     var (
         influxSlices []string
         err          error
         p            *influxdb2.Point
-        v            Message
+        v            *Message
     )
     influxSlices = strings.Split(w.influxDBDSN, "@")
 
@@ -137,20 +167,20 @@ func (w *WriteToFluxDB) Write(wc chan Message) {
             panic(err.Error())
         }
 
-        fmt.Println(v) // 获取WriteChan的数据直接打印出来
+        // fmt.Println(v) // 获取WriteChan的数据直接打印出来
     }
 }
 
 // 日志解析结构体
 type LogProcess struct {
-    rc    chan []byte  // 读取channel
-    wc    chan Message // 写入channel
+    rc    chan []byte   // 读取channel
+    wc    chan *Message // 写入channel
     read  Reader
     write Writer
 }
 
 // LogProcess结构体构造函数
-func NewLogProcess(rc chan []byte, wc chan Message, read Reader, write Writer) *LogProcess {
+func NewLogProcess(rc chan []byte, wc chan *Message, read Reader, write Writer) *LogProcess {
     return &LogProcess{
         rc:    rc,
         wc:    wc,
@@ -177,7 +207,7 @@ func (l *LogProcess) Process() {
         v       []byte
         regex   *regexp.Regexp
         result  []string
-        message = Message{}
+        message = &Message{}
 
         loc, _    = time.LoadLocation("Asia/Shanghai")
         timeLocal time.Time
@@ -205,15 +235,18 @@ func (l *LogProcess) Process() {
         result = regex.FindStringSubmatch(string(v))
 
         if len(result) != 14 {
+            TypeMonitorChan <- TypeErrorNumber
             log.Printf("FindStringSubmatch fail: %s", v)
             continue
         }
         if timeLocal, err = time.ParseInLocation("02/Jan/2006:15:04:05 +0000", result[4], loc); err != nil { // 时间
+            TypeMonitorChan <- TypeErrorNumber
             log.Printf("ParseInLocation fail: %s, got %s", err.Error(), result[4])
         }
         message.TimeLocal = timeLocal
 
         if byteSent, err = strconv.Atoi(result[8]); err != nil { // 发送字节数
+            TypeMonitorChan <- TypeErrorNumber
             log.Printf("Atoi fail: %s, got %s", err.Error(), result[8])
             continue
         }
@@ -222,11 +255,13 @@ func (l *LogProcess) Process() {
         splitSlice = strings.Split(result[6], " ")
 
         if len(splitSlice) != 3 { // 请求方法
+            TypeMonitorChan <- TypeErrorNumber
             log.Printf("strings.Split fail %s", result[6])
             continue
         }
         message.Method = splitSlice[0]
         if urlParse, err = url.Parse(splitSlice[1]); err != nil { // 请求地址
+            TypeMonitorChan <- TypeErrorNumber
             log.Printf("url parse fail: %s", err.Error())
             continue
         }
@@ -236,11 +271,13 @@ func (l *LogProcess) Process() {
         message.Status = result[7]
 
         if upstreamTime, err = strconv.ParseFloat(result[12], 64); err != nil {
+            TypeMonitorChan <- TypeErrorNumber
             log.Printf("upstreamTime ParseFloat fail: %s", err.Error())
             continue
         }
         message.UpstreamTime = upstreamTime
         if requestTime, err = strconv.ParseFloat(result[13], 64); err != nil {
+            TypeMonitorChan <- TypeErrorNumber
             log.Printf("requestTime ParseFloat fail: %s", err.Error())
             continue
         }
@@ -248,4 +285,94 @@ func (l *LogProcess) Process() {
 
         l.wc <- message // 将ReadChan中的数据获取并转化成大写传递给WriteChan
     }
+}
+
+// 系统状态监控
+type SystemInfo struct {
+    HandleLine   int     `json:"handleLine"`   // 总处理日志行
+    Tps          float64 `json:"tps"`          // 系统吞出量
+    ReadChanLen  int     `json:"readChanLen"`  // 读Channel 长度
+    WriteChanLen int     `json:"writeChanLen"` // 写Channel 长度
+    RunTime      string  `json:"runTime"`      // 运行总时间
+    ErrorNumber  int     `json:"errNumber"`    // 错误数
+}
+
+type TypeMonitor int
+
+// 辅助处理行数和错误行数
+const (
+    TypeHandleLine TypeMonitor = iota
+    TypeErrorNumber
+)
+
+var TypeMonitorChan = make(chan TypeMonitor, 200)
+
+type Monitor struct {
+    startTime time.Time
+    tpsSlice  []int
+    data      SystemInfo
+}
+
+// 系统监控构造函数
+func NewMonitor(startTime time.Time, data SystemInfo) *Monitor {
+    return &Monitor{
+        startTime: startTime,
+        data:      data,
+    }
+}
+
+func (m *Monitor) start(lp *LogProcess) {
+    var (
+        n      TypeMonitor
+        ticker <-chan time.Time
+    )
+    // 消费处理行数和错误行数
+    go func() {
+        for n = range TypeMonitorChan {
+            switch n {
+            case TypeErrorNumber:
+                m.data.ErrorNumber += 1
+            case TypeHandleLine:
+                m.data.HandleLine += 1
+            }
+        }
+    }()
+
+    ticker = time.Tick(time.Second * 5)
+
+    go func() {
+        for {
+            <-ticker
+            m.tpsSlice = append(m.tpsSlice, m.data.HandleLine)
+            if len(m.tpsSlice) > 2 {
+                m.tpsSlice = m.tpsSlice[1:]
+            }
+        }
+    }()
+
+    http.HandleFunc("/monitor", func(w http.ResponseWriter, r *http.Request) {
+        var (
+            result []byte
+            err    error
+        )
+        m.data.RunTime = time.Now().Sub(m.startTime).String()
+        m.data.ReadChanLen = len(lp.rc)
+        m.data.WriteChanLen = len(lp.wc)
+
+        if len(m.tpsSlice) >= 2 {
+            m.data.Tps = float64((m.tpsSlice[1] - m.tpsSlice[0]) / 5)
+        }
+
+        if result, err = json.MarshalIndent(m.data, "", "\t"); err != nil {
+            fmt.Printf("MarshalIndent failed: %s", err.Error())
+            return
+        }
+
+        if _, err = io.WriteString(w, string(result)); err != nil {
+            fmt.Printf("WriteString failed: %s", err.Error())
+            return
+        }
+    })
+
+    log.Fatal(http.ListenAndServe(":9191", nil))
 }
