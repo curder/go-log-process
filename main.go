57aa67f077f2dc9c1429043dd9e44b33f3d25621
curder <curder@foxmail.com>
2020-04-22 17:35:11 +0800

    read and wirte interface

--- main.go
+++ main.go
@@ -9,51 +9,85 @@ import (
 func main() {
     var (
         lp *LogProcess
+        r  Reader
+        w  Writer
     )
-    lp = NewLogProcess("/tmp/access.log", "username@password")
+    r = NewReadFromFile("./tmp/access.log")
+    w = NewWriteToFluxDB("username@password")
+    lp = NewLogProcess(r, w)
 
-    go lp.ReadFromFile()
+    go lp.read.Read(lp.rc)
     go lp.Process()
-    go lp.WriteToFluxDB()
+    go lp.write.Write(lp.wc)
 
     time.Sleep(time.Second)
 }
 
-// 日志解析结构体
-type LogProcess struct {
-    ReadChan    chan string // 读取channel
-    WriteChan   chan string // 写入channel
-    Path        string      // 读取文件的路径
-    InfluxDBDSN string      // influxDB连接地址
+// 读取接口
+type Reader interface {
+    Read(rc chan string)
 }
 
-// LogProcess结构体构造函数
-func NewLogProcess(path, influxDBDNS string) *LogProcess {
-    return &LogProcess{
-        ReadChan:    make(chan string),
-        WriteChan:   make(chan string),
-        Path:        path,
-        InfluxDBDSN: influxDBDNS,
+type ReadFromFile struct {
+    path string // 文件读取的地址
+}
+
+// 构造函数
+func NewReadFromFile(path string) *ReadFromFile {
+    return &ReadFromFile{
+        path: path,
     }
 }
 
-// 读取模块，从文件中读取
-func (l *LogProcess) ReadFromFile() {
+// 读取模块
+func (r *ReadFromFile) Read(rc chan string) {
     var (
-        line string
+        line = "message"
     )
-    line = "message"
-    // 模拟往 ReadChan中写入一行数据
-    l.ReadChan <- line
+    rc <- line // 模拟往 ReadChan中写入一行数据
+}
+
+// 写入接口
+type Writer interface {
+    Write(wc chan string)
+}
+
+// 写入到fluxDB
+type WriteToFluxDB struct {
+    influxDBDSN string // influxDB连接地址
+}
+
+// 构造函数
+func NewWriteToFluxDB(influxDBDNS string) *WriteToFluxDB {
+    return &WriteToFluxDB{
+        influxDBDSN: influxDBDNS,
+    }
+}
+
+// 写入模块
+func (w *WriteToFluxDB) Write(wc chan string) {
+    fmt.Println(<-wc) // 获取WriteChan的数据直接打印出来
+}
+
+// 日志解析结构体
+type LogProcess struct {
+    rc    chan string // 读取channel
+    wc    chan string // 写入channel
+    read  Reader
+    write Writer
+}
+
+// LogProcess结构体构造函数
+func NewLogProcess(read Reader, write Writer) *LogProcess {
+    return &LogProcess{
+        rc:    make(chan string),
+        wc:    make(chan string),
+        read:  read,
+        write: write,
+    }
 }
 
 // 解析模块，解析文件内容
 func (l *LogProcess) Process() {
-    // 将ReadChan中的数据获取并转化成大写传递给WriteChan
-    l.WriteChan <- strings.ToUpper(<-l.ReadChan)
-}
-
-// 写入模块，将解析好的内容写入到fluxDB
-func (l *LogProcess) WriteToFluxDB() {
-    fmt.Println(<-l.WriteChan) // 获取WriteChan的数据直接打印出来
+    l.wc <- strings.ToUpper(<-l.rc) // 将ReadChan中的数据获取并转化成大写传递给WriteChan
 }
